b0aa3e029bdef23dfebf88f3f78b0b206a16fefd
Jan Acke <jan.acke@student.hogent.be>
2012-04-11 17:00:22 +0200

    removed unused files

--- puppet/modules/java/lib/facter/javaexists.rb
+++
@@ -1,5 +0,0 @@
-Facter.add("javaexists") do
-  setcode do
-    File::exists?("/opt/jdk1.6.0_31/bin/java")
-  end
-end
--- puppet/modules/java/lib/facter/nfsdir.rb
+++
@@ -1,6 +0,0 @@
-
-Facter.add("nfsdir") do 
-  setcode do 
-    Facter::util::Resolution.exec('/bin/uname')
-  end
-end
