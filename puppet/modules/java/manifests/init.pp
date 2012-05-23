1694d1198a4f6bcf719ee6185a4b9478f0076c60
Jan Acke <jan.acke@student.hogent.be>
2012-05-23 14:03:35 +0200

    openjdk support

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -12,9 +12,9 @@ class java {
 }
 
 
-#mogelijk nog problemen met JAVA_HOME
 class java::openjdk {
-
+  require apt
+  
   package { "openjdk-6-jdk":
     ensure => latest,
   }
@@ -32,11 +32,18 @@ class java::install {
     group => root,
     source => "puppet:///modules/java/${filename}",
   }
+
+  #creating the directory where we want to install java
+  exec { "create_java_dir":
+    command => "mkdir -p ${java::environment::install_path}",
+    onlyif => "test ! -s ${java::environment::install_path}/bin",
+    require => File["/tmp/${filename}"],
+  }
   
   exec { "execute_bin":
     command => "/tmp/${filename}",
-    require => File["/tmp/${filename}"],
-    onlyif => "test ! -s ${java::environment::install_path}/bin",
+    subscribe => Exec["create_java_dir"],
+    refreshonly => true,
     cwd => "/tmp",
   }
 
@@ -44,7 +51,7 @@ class java::install {
   #the default name :/. Also at first this was subsribed to the execute_bin
   #resoource but sometimes this would not be executed immediately after the
   #execute_bin, which is what we want
-  exec { "mv /tmp/jdk1.6* ${java::environment::install_path}":
+  exec { "mv /tmp/jdk1.6*/* ${java::environment::install_path}":
     refreshonly => true,
     subscribe => Exec["execute_bin"],
   }
