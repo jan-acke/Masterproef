a17ab00031d0deecd88f6b0ecd7dbf406a659bef
Jan Acke <jan.acke@student.hogent.be>
2012-05-24 16:54:06 +0200

    slightly more verbose scripts

--- scripts/startall.sh
+++ scripts/startall.sh
@@ -3,12 +3,30 @@
 #start services, followed guidelines on 
 #https://ccp.cloudera.com/display/CDHDOC/CDH3+Deployment+on+a+Cluster
 
+
+echo "Starting hadoop namenode"
 mco service -C cdh3::hadoop::namenode hadoop-0.20-namenode start
+
+echo "Starting hadoop datanode(s)"
 mco service -C cdh3::hadoop::datanode hadoop-0.20-datanode start
+
+echo "Starting hadoop jobtracker"
 mco service -C cdh3::hadoop::tasktracker hadoop-0.20-tasktracker start
+
+echo "Starting hadoop tasktracker(s)"
 mco service -C cdh3::hadoop::jobtracker hadoop-0.20-jobtracker start
+
+echo "Starting zookeeper(s)"
 mco service -C cdh3::zookeeper hadoop-zookeeper-server start
+
+echo "Starting hbase master"
 mco service -C cdh3::hbase::master hadoop-hbase-master start
+
+echo "Starting hbase regionserver(s)"
 mco service -C cdh3::hbase::regionserver hadoop-hbase-regionserver start
+
+echo "Starting Lily solr"
 mco service -C lily::solr lily-solr start
+
+echo "Starting Lily"
 mco service -C lily::server lily-server start
--- scripts/stopall.sh
+++ scripts/stopall.sh
@@ -3,14 +3,32 @@
 #stop services, followed guidelines on 
 #https://ccp.cloudera.com/display/CDHDOC/CDH3+Deployment+on+a+Cluster
 
+
+echo "Stopping Lily"
 mco service -C lily::server lily-server stop
+
+echo "Stopping Lily solr"
 mco service -C lily::solr lily-solr stop
+
+echo "Stopping hbase master"
 mco service -C cdh3::hbase::master hadoop-hbase-master stop
+
+echo "Stopping hbase regionserver(s)"
 mco service -C cdh3::hbase::regionserver hadoop-hbase-regionserver stop
+
+echo "Stopping zookeeper(s)"
 mco service -C cdh3::zookeeper hadoop-zookeeper-server stop
+
+echo "Stopping hadoop jobtracker"
 mco service -C cdh3::hadoop::jobtracker hadoop-0.20-jobtracker stop
+
+echo "Stopping hadoop tasktracker(s)"
 mco service -C cdh3::hadoop::tasktracker hadoop-0.20-tasktracker stop
+
+echo "Stopping hadoop namenode"
 mco service -C cdh3::hadoop::namenode hadoop-0.20-namenode stop
+
+echo "Stopping hadoop datanode(s)"
 mco service -C cdh3::hadoop::datanode hadoop-0.20-datanode stop
 
 
